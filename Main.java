cfbd25258c8c4f553dcd3045712fc8aaf236ca83
Sawera09 <sawerakamal38@gmail.com>
2024-05-22 17:13:29 +0500

    Add files via upload
    qno:01 quick sort

---
+++ Main.java
@@ -0,0 +1,38 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] data = {2, 8, 7, 1, 3, 5, 6, 4};
+        quickSort(data, 0, data.length - 1);
+        System.out.print("Sorted data: ");
+        for (int num : data) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+}
